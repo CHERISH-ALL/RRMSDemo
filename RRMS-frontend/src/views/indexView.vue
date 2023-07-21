43492e22943a592da5496dd3a93dab6ec0ff9467
CHERISH-ALL <cherish-all@example.com>
2023-07-21 12:03:23 +0800

    2023.7.21,part3

--- RRMS-frontend/src/views/indexView.vue
+++ RRMS-frontend/src/views/indexView.vue
@@ -1,7 +1,7 @@
 <!--登录完成的页面设计-->
 <template>
   <div style="width :100vw; height: 100vh; overflow:hidden">
-    <div style="width: 100vw;height: 10vh; overflow:hidden;background-color: #545c64">
+    <div style="width: 100vw;height: 10vh;background-color: #545c64">
       <div style="float:left;width: 25vw;height: 10vh">
         <el-image fit="cover" src="./src/assets/login/gxunLogo.png"
                   style="max-width: 100%;height:100%;object-fit: cover"/>
@@ -28,12 +28,132 @@
       </div>
 
     </div>
-    <div style="width: 100vw;height: 100vh">
-      <router-view v-slot="{ Component }">
-        <transition mode="out-in" name="el-fade-in-linear">
-          <component :is="Component"/>
-        </transition>
-      </router-view>
+    <div style="width: 100vw;height: 90vh">
+      <div style="float:left;width: 15vw;height: 90vh">
+        <el-menu
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            class="el-menu-vertical-demo"
+            style="height: 100vh;width: 15vw;"
+            text-color="#fff"
+            unique-opened="unique-opened"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <span>个人信息</span>
+            </template>
+            <el-menu-item index="1-1">
+              <span>查询个人信息</span>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+              <span>修改个人信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <span>学生信息</span>
+            </template>
+            <el-sub-menu index="2-1">
+              <template #title>
+                <span>查询学生信息</span>
+              </template>
+              <el-menu-item index="2-1-1">按项目</el-menu-item>
+              <el-menu-item index="2-1-2">按学生</el-menu-item>
+            </el-sub-menu>
+            <el-menu-item :disabled="isStudent||isTeacher" index="2-2">
+              <span>修改学生信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <span>日志信息</span>
+            </template>
+            <el-menu-item index="3-1">
+              <span>按科研室查询</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <span>按学生查询</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu :disabled="isStudent" index="4">
+            <template #title>
+              <span>提交或处理申请</span>
+            </template>
+            <el-menu-item :disabled="isStudent||isAdmin" index="4-1">
+              <span>提交设备申请</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent||isTeacher" index="4-2">
+              <span>处理设备申请</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent||isAdmin" index="4-3">
+              <span>提交项目申请</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent||isTeacher" index="4-4">
+              <span>处理项目申请</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="5">
+            <template #title>
+              <span>论文与专利信息</span>
+            </template>
+            <el-menu-item index="5-1">
+              <span>查询论文与专利信息</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent" index="5-2">
+              <span>修改论文与专利信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="6">
+            <template #title>
+              <span>科研项目信息</span>
+            </template>
+            <el-menu-item index="6-1">
+              <span>查询科研项目</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent" index="6-2">
+              <span>修改科研项目</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="7">
+            <template #title>
+              <span>指导老师信息</span>
+            </template>
+            <el-sub-menu index="7-1">
+              <template #title>
+                <span>查询指导老师信息</span>
+              </template>
+              <el-menu-item index="7-1-1">按项目</el-menu-item>
+              <el-menu-item index="7-1-2">按老师</el-menu-item>
+            </el-sub-menu>
+            <el-menu-item :disabled="isStudent||isTeacher" index="7-2">
+              <span>修改指导老师信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="8">
+            <template #title>
+              <span>设备信息</span>
+            </template>
+            <el-menu-item index="8-1">
+              <span>查询设备信息</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent||isTeacher" index="8-2">
+              <span>修改设备信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="9">
+            <template #title>
+              <span>科研室信息</span>
+            </template>
+            <el-menu-item index="9-1">
+              <span>查询科研室信息</span>
+            </el-menu-item>
+            <el-menu-item :disabled="isStudent||isTeacher" index="9-2">
+              <span>修改科研室信息</span>
+            </el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </div>
+      <div style="float:left;width: 85vw;height: 90vh;background-color:red">123</div>
     </div>
   </div>
 
@@ -46,8 +166,12 @@ import {ElMessage} from "element-plus";
 import {useStore} from "@/stores";
 import {get} from "@/net";
 
-
 const store = useStore();
+
+const isStudent = store.auth.user.identity === 'student';
+const isTeacher = store.auth.user.identity === 'teacher';
+const isAdmin = store.auth.user.identity === 'admin';
+
 
 const logout = () => {
   get("/api/auth/logout", () => {
@@ -55,10 +179,29 @@ const logout = () => {
     store.auth.user = 'null';
     router.push('/');
   })
-
 }
+
 </script>
 
-<style scoped>
+<!--<script>
+import {useStore} from "@/stores";
 
+const store = useStore();
+export default{
+  methods:{
+    myFunction(){
+      if(store.auth.user.identity.value().equals('admin')){
+        isChangeStudent.value=true;
+      }
+    }
+  },
+  mounted() {
+    this.myFunction();
+  }
+}
+
+
+</script>-->
+
+<style scoped>
 </style>
